613ddc764a3dce310b9ecf5125498dcbc13cd03e
Sebastian Cabal <sebascabalq@gmail.com>
2023-10-23 17:06:00 -0500

    Boton de menu

--- upperside/src/components/BurguerButton..jsx
+++ upperside/src/components/BurguerButton..jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import UserProfile from '../assets/IconoPerfil.svg';
 import ShoppingBag from '../assets/IconoBolsa.svg';
 import IconSearch from '../assets/IconoLupa.svg';
@@ -7,6 +7,10 @@ import '../styles/Burguer.css';
 
 
 function BurguerButton() {
+    const [submenuVisible, setSubmenuVisible] = useState(false);
+    const handleIconClick = () => {
+        setSubmenuVisible(!submenuVisible);
+    };
     return (
         <div className='en-efecto'>
             <div className="IconsMenu">
@@ -15,11 +19,19 @@ function BurguerButton() {
                 <img src={UserProfile} alt="Icono de Perfil" className="icon-navbar" />
                 <img src={ShoppingBag} alt="Icono de Carrito Compras" className="icon-navbar" />
             </div>
-            <div className='BurguerMenu'>
+            <div className='BurguerMenu' onClick={handleIconClick}>
                 <div className="icon nav-icon-5">
                     <span></span>
                     <span></span>
                     <span></span>
+                    {submenuVisible && (
+                        <ul>
+                            <li>Colecciones</li>
+                            <li>Lencería</li>
+                            <li>Inferiores</li>
+                            <li>Complementos</li>
+                        </ul>
+                    )}
                 </div>
             </div>
         </div>
