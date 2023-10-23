29dbed1d7e7f9c25aaf130a5cc65de6c10db6756
Sebastian Cabal <sebascabalq@gmail.com>
2023-10-23 15:27:56 -0500

    Estilo subrayado en el navbar

---
+++ upperside/src/components/BurguerButton.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function BurguerButton() {
+  return (
+    <>
+    BurguerButton
+    </>
+  )
+}
+
+export default BurguerButton
--- upperside/src/components/Navbar.jsx
+++ upperside/src/components/Navbar.jsx
@@ -1,6 +1,10 @@
 import React from "react";
 import logo from "../assets/LogoUpperside.png";
 import "./Navbar.css"; // Estilos CSS para la barra de navegación
+import UserProfile from '../assets/IconoPerfil.svg';
+import ShoppingBag from '../assets/IconoBolsa.svg';
+import IconSearch from '../assets/IconoLupa.svg';
+import FavoriteIcon from '../assets/IconoCorazón.svg';
 
 const Navbar = () => {
     return (
@@ -23,6 +27,11 @@ const Navbar = () => {
                 </a>
             </div>
             <div className="navbar-right">
+            <img src={IconSearch} alt="Icono de Busqueda" className="icon-navbar"  />
+            <img src={FavoriteIcon} alt="Icono de Corazon" className="icon-navbar"  />
+            <img src={UserProfile} alt="Icono de Perfil" className="icon-navbar"  />
+            <img src={ShoppingBag} alt="Icono de Carrito Compras" className="icon-navbar"  />
+            
 
             </div>
         </nav>
