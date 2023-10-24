0c0dcba9d63ea61f0879d2ed0849ce611b021161
Skabas18 <sebascabalq@gmail.com>
2023-10-24 17:59:44 -0500

    Navegacion lista

--- upperside/src/App.js
+++ upperside/src/App.js
@@ -1,5 +1,5 @@
 import './styles/App.css';
-import { Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import Navbar from './components/Navbar';
 import Inicio from './pages/Home';
 import Colecciones from './pages/Colecciones';
@@ -7,21 +7,20 @@ import Lenceria from './pages/Lenceria';
 import Inferiores from './pages/Inferiores';
 import Complementos from './pages/Complementos';
 
-
-
 function App() {
   return (
-    <>
+    <Router>
       <Navbar />
       <Routes>
-        <Route path="/" component={Inicio} />
-        <Route path="/Colecciones" component={Colecciones} />
-        <Route path="/Lenceria" component={Lenceria} />
-        <Route path="/Inferiores" component={Inferiores} />
-        <Route path="/Complementos" component={Complementos} />
+        <Route path="/" element={<Inicio/>} />
+        <Route path="/Colecciones" element={<Colecciones/>} />
+        <Route path="/Lenceria" element={<Lenceria/>} />
+        <Route path="/Inferiores" element={<Inferiores/>} />
+        <Route path="/Complementos" element={<Complementos/>} />
       </Routes>
-    </>
+    </Router>
   );
 }
+
 
 export default App;
