0d90d577e6dc38138b21405c7e567eac808293bd
Sebastian Cabal <sebascabalq@gmail.com>
2023-10-26 15:34:41 -0500

    Estilos del footer

--- upperside/src/components/Footer.jsx
+++ upperside/src/components/Footer.jsx
@@ -1,7 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react'
 import logo from "../assets/LogoUppersideBlaco.png"
+import Whatsapp from "../assets/Iconos/Whatsapp.svg"
+import Tiktok from "../assets/Iconos/TikTok.svg"
+import Instagram from "../assets/Iconos/Instagram.svg"
+import Youtube from "../assets/Iconos/Youtube.svg"
 import '../styles/Footer.css'
 function Footer() {
+    const [correoElectronico, setCorreoElectronico] = useState('');
+
+    const handleInputChange = (event) => {
+        setCorreoElectronico(event.target.value);
+    };
+
+    const handleRegistrarseClick = () => {
+        // Aquí puedes realizar acciones como enviar el correo electrónico a un servidor
+        console.log('Correo electrónico registrado:', correoElectronico);
+    };
+
     return (
         <div className='footer'>
             <div className='footer-section-padding'>
@@ -9,7 +24,7 @@ function Footer() {
                     <div className='footer-links-div'>
                         <img src={logo} alt="" />
                         <a href="https://www.google.com/">
-                            <p>Plítica y privacidad</p>
+                            <p>Política y privacidad</p>
                             <p>Términos y condiciones</p>
                         </a>
                         <hr />
@@ -18,11 +33,43 @@ function Footer() {
                         </p>
                         <hr />
                         <p>Siguenos en nuestras redes sociales</p>
+                        <div className='socialmedia'>
+                            <a href="https://www.google.com/">
+                                <img src={Instagram} alt="" />
+                            </a>
+                            <a href="https://www.google.com/">
+                                <img src={Whatsapp} alt="" />
+                            </a>
+                            <a href="https://www.google.com/">
+                                <img src={Tiktok} alt="" />
+                            </a>
+                            <a href="https://www.google.com/">
+                                <img src={Youtube} alt="" />
+                            </a>
+                        </div>
 
                     </div>
                     <div className='footer-links-div'>
                         <h4>Trabaja con nosotros</h4>
                         <p>¿Estas interesad@ en hacer parte del equipo Upper?</p>
+                        <p>Comunícate con:
+                            info@upperside.co
+                        </p>
+                        <p>PBX
+                            +(57) 602 8912397
+                        </p>
+                    </div>
+                    <div className='footer-links-div'>
+                        <h4>¡Mantente informad@ sobre Upperside!</h4>
+                        <div className="registro-formulario">
+                            <input
+                                type="email"
+                                placeholder="Correo electrónico"
+                                value={correoElectronico}
+                                onChange={handleInputChange}
+                            />
+                            <button onClick={handleRegistrarseClick}>Registrarse</button>
+                        </div>
                         <p>Comunícate con:
                             info@upperside.co
                         </p>
