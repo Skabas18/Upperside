1171f0524d5f30518796c8114f338ab600e886b0
Sebastian Cabal <sebascabalq@gmail.com>
2023-10-27 07:49:08 -0500

    Slider vertical

--- upperside/src/components/CustomSlider.jsx
+++ upperside/src/components/CustomSlider.jsx
@@ -12,26 +12,23 @@ function CustomSlider({ data }) {
     const prevSlide = (e) => {
         e.stopPropagation();
         setSlide(slide === 0 ? data.length - 1 : slide - 1);
-        console.log("E =>"+slide);
     };
 
-    
     return (
         <div className="carousel">
             <div className="slider-container">
                 {data.map((item, idx) => (
-                    <div className="slide-container" key={idx}>
-                        <img
-                            src={item.src}
-                            alt={item.alt}
-                            className={slide === idx ? "slide" : "slide slide-hidden"}
-                        />
-                        <div className="arrows">
-                            <BsArrowUpCircleFill onClick={prevSlide} className="arrow arrow-up" />
-                            <BsArrowDownCircleFill onClick={nextSlide} className="arrow arrow-down" />
-                        </div>
-                    </div>
+                    <img
+                        src={item.src}
+                        alt={item.alt}
+                        key={idx}
+                        className={slide === idx ? "slide" : "slide slide-hidden"}
+                    />
                 ))}
+            </div>
+            <div className="arrows">
+                <BsArrowUpCircleFill onClick={prevSlide} className="arrow arrow-up" />
+                <BsArrowDownCircleFill onClick={nextSlide} className="arrow arrow-down" />
             </div>
         </div>
     );
